dda050296fc8ce33004c239d61ac4e9fac216a7a
Bangwahyu14 <whagung553@gmail.com>
2024-04-26 14:26:54 +0700

    membuat nodebaru dan previous

--- test/test.cpp
+++ test/test.cpp
@@ -23,7 +23,13 @@ void addNode() {
 	if (STAR == NULL || nim <= STAR->noMhs) {
 		if (STAR != NULL && nim == STAR->noMhs)
 		{
-
+			cout << "NIM sudah ada" << endl;
+			return;
 		}
+		previous = current;
+		current = current->next
 	}
+
+	nodeBaru->next = current;
+	previous->next = nodeBaru;
 }
