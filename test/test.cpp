73bc15979c813e806fc118163e0e15f5904c895a
Bangwahyu14 <whagung553@gmail.com>
2024-04-26 15:23:23 +0700

    selesai

--- test/test.cpp
+++ test/test.cpp
@@ -89,3 +89,97 @@ void raverse() {
 }
 
 
+void searchData() {
+	if (listEmpty()) {
+		cout << "List Kosong" << endl;
+		system(”pause”);
+		system(”cls“);
+		return;
+	}
+	else {
+		int nim;
+		cin >> nim;
+		cout << "Masukkan NIM"
+		Node* currentNode = START;
+		while (currentNode != NULL) {
+			if (currentNode->noMhs	nim) {
+				cout << “NIM: << currentNode->noMhs << “, üama : << currentNode->name << endl;
+				return;
+			}
+			currentNode = currentNode->next;
+		}
+		cout << "Data tiöak dîtemukan" << endl;
+	}
+}
+int main() {
+	int	pilihan;
+	do
+	{
+		try
+		{
+
+			cout << "l.	Tambah Data" << endl;
+			cout << "2.	Hapus Data" ‹ < endl;
+			cout << "3.	Tampilkan Data" << endl;
+			cout << "4.	Cari Data" << endl;
+			cout << "5.	Keluar" << endl;
+			cout << "pilihan: ";
+			cin >> pilihan;
+			swict(pilihan)
+			{
+			case 1:
+				addNode();
+				cout << "Data Berhasil ditambahkan" << endl;
+				system(”pause”);
+				system(”cls“);
+				break;
+			case 2:
+				if (listEmpty())
+				{
+					cout << "List kosong" << endl;
+					system(“pause“);
+					system(“mls”);
+					break;
+				}
+
+				int nim;
+
+				cout << "Masukkan NIM: ";
+				cin >> nim;
+				if (deleteNode(nim)) {
+					cout << “nim: ” << nim << ” berhasil ‹]ihapus” << endl;
+					system(”pause”);
+					system(”cls”);
+				}
+				else
+					cout «	"Data tidak ditemukan ” «	endl ;
+					break;
+			case 3:
+				traverse();
+				break;
+			case 4:
+				searchData();
+				break;
+			case 5:
+				break;
+			default:
+				cout << “Pilihan tidak ada” << endl;
+				break;
+			}
+		}
+		catch (exception e)
+		{
+			cout << "Terjadi kesalahan" << endl;
+		}
+
+	} while (pilihan != 5);
+}
+
+
+
+
+
+
+
+
+
